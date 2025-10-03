87636002b22a7401fe6113936862fc99ad7a2424
DarkProgrammer66 <mohamad.ali1860@guru.smk.belajar.id>
2025-10-03 16:12:05 +0700

    assertion, skip, before and after

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -2,8 +2,41 @@ package helper
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestMain(m *testing.M) {
+	fmt.Println("Sebelum Unit Test")
+
+	m.Run() // eksekusi semua unit test
+
+	fmt.Println("Setelah Unit Test")
+}
+
+func TestHelloWorldRequire(t *testing.T) {
+	result := HelloWorld("Ali")
+	require.Equal(t, "Hello Ali", result, "Result must be 'Hello Ali'")
+	fmt.Println("TestHelloWorld with Require Done")
+}
+
+func TestHelloWorldAssert(t *testing.T) {
+	result := HelloWorld("Ali")
+	assert.Equal(t, "Hello Ali", result, "Result must be 'Hello Ali'")
+	fmt.Println("TestHelloWorld with Assert Done")
+}
+
+func TestSkip(t *testing.T){
+	if runtime.GOOS == "windows" {
+		t.Skip("Unit test tidak bisa jalan di Windows")
+	}
+
+	result := HelloWorld("Ali")
+	require.Equal(t, "Hello Ali", result)
+}
 
 func TestHelloWorldAli(t *testing.T) {
 	result := HelloWorld("Ali")
